5a1e8407d8791d8fbdde6d1c7df0f8d273d6fc6f
Rana <aimotionbox@gmail.com>
2026-01-27 23:53:54 +0530

    changing model name.

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -72,7 +72,7 @@ export const createUserProject = async (req: Request, res: Response) => {
 
         // Enhance user prompt
         const promptEnhanceResponse = await openai.chat.completions.create({
-            model: 'google/gemini-2.0-flash-lite-preview-02-05:free',
+            model: 'kwaipilot/kat-coder-pro:free',
             messages: [
                 {
                     role: 'system',
@@ -116,7 +116,7 @@ export const createUserProject = async (req: Request, res: Response) => {
 
         // Generate website code
         const codeGenerationResponse = await openai.chat.completions.create({
-            model: 'google/gemini-2.0-flash-lite-preview-02-05:free',
+            model: 'kwaipilot/kat-coder-pro:free',
             messages: [
                 {
                     role: 'system',
@@ -205,9 +205,7 @@ export const createUserProject = async (req: Request, res: Response) => {
             data: {credits: {increment: 5}}
         })
         console.log(error);
-        if (!res.headersSent) {
-            res.status(500).json({ message: error.message });
-        }
+        res.status(500).json({ message: error.message });
     }
 }
 
